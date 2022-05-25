2357780976ff476f2a3dfbd09fbcc0104e6fb4d5
AREXANS <89494133+AREXANS@users.noreply.github.com>
2022-05-25 16:38:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ global.APIs = {
 
 // Free apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz' : '8BNUgTSi', // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://api.zeeoneofc.xyz' : 'OtbJUWtV', // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 global.self_mode = false
